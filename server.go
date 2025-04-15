13d1a0d07d2eb81902b786e18f1e083cfaacef03
Harry Pidcock <harry.pidcock@canonical.com>
2025-04-15 20:49:38 +1000

    fix: dedup covdata meta files

--- server.go
+++ server.go
@@ -14,6 +14,7 @@ import (
 	"io/fs"
 	"log"
 	"log/slog"
+	"maps"
 	"net/http"
 	"os"
 	"os/exec"
@@ -23,7 +24,6 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
-	"golang.org/x/sync/errgroup"
 )
 
 type server struct {
@@ -35,9 +35,7 @@ type server struct {
 
 	mut             sync.Mutex
 	writingProfiles map[uuid.UUID]chan struct{}
-	needsCompact    map[string]bool
-
-	closedChan <-chan struct{}
+	needsCompact    map[string]int
 }
 
 func newServer(dataDir string, psk []byte) *server {
@@ -48,8 +46,7 @@ func newServer(dataDir string, psk []byte) *server {
 		dataDir:         dataDir,
 		dataDirFS:       os.DirFS(dataDir),
 		writingProfiles: make(map[uuid.UUID]chan struct{}),
-		needsCompact:    make(map[string]bool),
-		closedChan:      closedChan,
+		needsCompact:    make(map[string]int),
 	}
 }
 
@@ -63,19 +60,9 @@ func (s *server) writing(namespaceToCompact string, profile uuid.UUID) func() {
 		close(s.writingProfiles[profile])
 		delete(s.writingProfiles, profile)
 		if namespaceToCompact != "" {
-			s.needsCompact[namespaceToCompact] = true
+			s.needsCompact[namespaceToCompact]++
 		}
 	}
-}
-
-func (s *server) reading(namespace string, profile uuid.UUID) <-chan struct{} {
-	s.mut.Lock()
-	defer s.mut.Unlock()
-	var ch <-chan struct{} = s.writingProfiles[profile]
-	if ch == nil {
-		ch = s.closedChan
-	}
-	return ch
 }
 
 func (s *server) Handler() http.Handler {
@@ -170,6 +157,13 @@ func (s *server) HandleUpload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	complete := false
+	defer func() {
+		if !complete {
+			_ = os.RemoveAll(dir)
+		}
+	}()
+
 	gzipReader, err := gzip.NewReader(r.Body)
 	if err != nil {
 		log.Printf("failed to create gzip reader: %v", err)
@@ -218,6 +212,7 @@ func (s *server) HandleUpload(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+	complete = true
 	http.Error(w, "OK", http.StatusOK)
 }
 
@@ -233,7 +228,7 @@ func (s *server) HandleDownload(w http.ResponseWriter, r *http.Request) {
 
 	s.cleanupMut.Lock()
 	defer s.cleanupMut.Unlock()
-	result, err := s.compactNamespace(context.Background(), namespace, dir, true)
+	result, err := s.compactNamespace(context.Background(), namespace, dir)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -259,12 +254,14 @@ func (s *server) HandleDownload(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+const minCompactCount = 32
+
 func (s *server) CompactableNamespaces() []string {
 	s.mut.Lock()
 	defer s.mut.Unlock()
 	var namespaces []string
-	for namespace, ok := range s.needsCompact {
-		if ok {
+	for namespace, count := range s.needsCompact {
+		if count > minCompactCount {
 			namespaces = append(namespaces, namespace)
 		}
 	}
@@ -280,21 +277,17 @@ func (s *server) CompactNamespace(ctx context.Context, namespace string) error {
 
 	s.cleanupMut.Lock()
 	defer s.cleanupMut.Unlock()
-	_, err = s.compactNamespace(ctx, namespace, dir, false)
+	_, err = s.compactNamespace(ctx, namespace, dir)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (s *server) compactNamespace(ctx context.Context, namespace string, dir string, force bool) (string, error) {
+func (s *server) compactNamespace(ctx context.Context, namespace string, dir string) (string, error) {
 	s.mut.Lock()
-	compactNeeded := s.needsCompact[namespace]
 	delete(s.needsCompact, namespace)
-	if !force && !compactNeeded {
-		s.mut.Unlock()
-		return "", nil
-	}
+	writingProfiles := maps.Clone(s.writingProfiles)
 	entries, err := os.ReadDir(dir)
 	s.mut.Unlock()
 	if err != nil {
@@ -311,13 +304,19 @@ func (s *server) compactNamespace(ctx context.Context, namespace string, dir str
 		if err != nil {
 			continue
 		}
-		select {
-		case <-s.reading(namespace, id):
-		case <-ctx.Done():
-			return "", ctx.Err()
+		if ch, ok := writingProfiles[id]; ok {
+			select {
+			case <-ch:
+			case <-ctx.Done():
+				return "", ctx.Err()
+			}
+			_, err := os.Stat(path.Join(dir, name))
+			if errors.Is(err, os.ErrNotExist) {
+				continue
+			} else if err != nil {
+				return "", err
+			}
 		}
-		unlock := s.writing("", id)
-		defer unlock()
 		profileDirs = append(profileDirs, name)
 	}
 	if len(profileDirs) == 0 {
@@ -327,7 +326,12 @@ func (s *server) compactNamespace(ctx context.Context, namespace string, dir str
 		return profileDirs[0], nil
 	}
 
-	result, err := s.compact(ctx, dir, profileDirs)
+	collected, err := s.collect(ctx, dir, profileDirs)
+	if err != nil {
+		return "", fmt.Errorf("failed to compact/collect namespace %q: %w", namespace, err)
+	}
+
+	result, err := s.compact(ctx, dir, collected)
 	if err != nil {
 		return "", fmt.Errorf("failed to compact namespace %q: %w", namespace, err)
 	}
@@ -336,48 +340,152 @@ func (s *server) compactNamespace(ctx context.Context, namespace string, dir str
 	return result, nil
 }
 
-const jobBranchNum = 8
-const maxProfilesPerJob = 128
+func (s *server) collect(ctx context.Context, dir string, profileDirs []string) (string, error) {
+	counters := map[string]string{}
+	meta := map[string]string{}
 
-func (s *server) compact(ctx context.Context, dir string, profileDirs []string) (string, error) {
-	if len(profileDirs) == 0 {
-		return "", nil
-	}
-	if len(profileDirs) > maxProfilesPerJob {
-		jobsPerBranch := len(profileDirs) / jobBranchNum
-		groups := [jobBranchNum][]string{}
-		for i := range groups {
-			if i == jobBranchNum-1 {
-				groups[i] = profileDirs
-			} else {
-				groups[i] = profileDirs[:jobsPerBranch]
-				profileDirs = profileDirs[jobsPerBranch:]
-			}
-		}
-
-		results := [jobBranchNum]string{}
-		eg, egCtx := errgroup.WithContext(ctx)
-		for i := range groups {
-			eg.Go(func() error {
-				var err error
-				results[i], err = s.compact(egCtx, dir, groups[i])
-				return err
-			})
-		}
-
-		err := eg.Wait()
+	for _, profile := range profileDirs {
+		profileDir := path.Join(dir, profile)
+		err := s.collectOne(ctx, profileDir, counters, meta)
 		if err != nil {
+			return "", fmt.Errorf("collecting profile %q", profile)
+		}
+	}
+
+	id, err := uuid.NewV7()
+	if err != nil {
+		return "", err
+	}
+
+	unlock := s.writing("", id)
+	defer unlock()
+
+	outDir := path.Join(dir, id.String())
+	err = os.Mkdir(outDir, 0755)
+	if err != nil {
+		return "", fmt.Errorf("failed to create directory: %w", err)
+	}
+
+	for metaHash, metaSource := range meta {
+		inMeta, err := os.Open(metaSource)
+		if err != nil {
+			_ = os.RemoveAll(outDir)
 			return "", err
 		}
 
-		profileDirs = []string{}
-		for _, v := range results {
-			if v != "" {
-				profileDirs = append(profileDirs, v)
-			}
+		metaPath := path.Join(outDir, "covmeta."+metaHash)
+		outMeta, err := os.Create(metaPath)
+		if err != nil {
+			_ = inMeta.Close()
+			_ = os.RemoveAll(outDir)
+			return "", err
+		}
+
+		_, err = io.Copy(outMeta, inMeta)
+		_ = outMeta.Close()
+		_ = inMeta.Close()
+		if err != nil {
+			_ = os.RemoveAll(outDir)
+			return "", err
 		}
 	}
 
+	for counterSource, counterName := range counters {
+		inCounter, err := os.Open(counterSource)
+		if err != nil {
+			_ = os.RemoveAll(outDir)
+			return "", err
+		}
+
+		counterPath := path.Join(outDir, counterName)
+		outCounter, err := os.Create(counterPath)
+		if err != nil {
+			_ = inCounter.Close()
+			_ = os.RemoveAll(outDir)
+			return "", err
+		}
+
+		_, err = io.Copy(outCounter, inCounter)
+		_ = outCounter.Close()
+		_ = inCounter.Close()
+		if err != nil {
+			_ = os.RemoveAll(outDir)
+			return "", err
+		}
+	}
+
+	for _, subDir := range profileDirs {
+		err := os.RemoveAll(path.Join(dir, subDir))
+		if err != nil {
+			slog.Error("failed to remove directory", "dir", subDir, "err", err)
+			continue
+		}
+	}
+
+	return id.String(), nil
+}
+
+func (s *server) collectOne(ctx context.Context, dir string, counters map[string]string, meta map[string]string) error {
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	mapper := map[string]string{}
+	for _, f := range entries {
+		if f.IsDir() {
+			continue
+		}
+		name := f.Name()
+		if !strings.HasPrefix(name, "covmeta.") {
+			continue
+		}
+		metaName := strings.TrimPrefix(name, "covmeta.")
+		metaPath := path.Join(dir, name)
+		metaFile, err := os.Open(metaPath)
+		if err != nil {
+			return err
+		}
+		hasher := sha256.New()
+		_, err = io.Copy(hasher, metaFile)
+		metaFile.Close()
+		if err != nil {
+			return err
+		}
+		metaHash := hex.EncodeToString(hasher.Sum(nil))
+		mapper[metaName] = metaHash
+		meta[metaHash] = metaPath
+	}
+
+	for _, f := range entries {
+		if f.IsDir() {
+			continue
+		}
+		name := f.Name()
+		if !strings.HasPrefix(name, "covcounters.") {
+			continue
+		}
+		nameParts := strings.Split(name, ".")
+		if len(nameParts) != 4 {
+			return fmt.Errorf("covcounter file with wrong name %q", name)
+		}
+		metaName := nameParts[1]
+		metaHash := mapper[metaName]
+		if metaHash == "" {
+			return fmt.Errorf("covcounter file missing matching meta %q", name)
+		}
+		counterPath := path.Join(dir, name)
+		nameParts[1] = metaHash
+		newName := strings.Join(nameParts, ".")
+		counters[counterPath] = newName
+	}
+
+	return nil
+}
+
+const maxProfilesPerJob = 2048
+
+func (s *server) compact(ctx context.Context, dir string, profileDir string) (string, error) {
 	id, err := uuid.NewV7()
 	if err != nil {
 		return "", err
@@ -399,7 +507,7 @@ func (s *server) compact(ctx context.Context, dir string, profileDirs []string) 
 		outDir = path.Join(wd, outDir)
 	}
 
-	cmd := exec.CommandContext(ctx, "go", "tool", "covdata", "merge", "-i="+strings.Join(profileDirs, ","), "-o="+outDir, "-pcombine")
+	cmd := exec.CommandContext(ctx, "go", "tool", "covdata", "merge", "-i="+profileDir, "-o="+outDir, "-pcombine")
 	cmd.Dir = dir
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -409,14 +517,9 @@ func (s *server) compact(ctx context.Context, dir string, profileDirs []string) 
 		return "", fmt.Errorf("failed to merge coverage data: %w", err)
 	}
 
-	s.mut.Lock()
-	defer s.mut.Unlock()
-	for _, subDir := range profileDirs {
-		err := os.RemoveAll(path.Join(dir, subDir))
-		if err != nil {
-			slog.Error("failed to remove directory", "dir", subDir, "err", err)
-			continue
-		}
+	err = os.RemoveAll(path.Join(dir, profileDir))
+	if err != nil {
+		slog.Error("failed to remove directory", "dir", profileDir, "err", err)
 	}
 
 	return id.String(), nil
